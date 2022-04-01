be0b0c2d3c02976cb4032308c59ab8b83e35f12e
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-01 02:42:21 +0200

    [Uuid] Properly generate a random Uuid

--- src/Uuid.cpp
+++ src/Uuid.cpp
@@ -1,15 +1,22 @@
-#pragma once
 #include "Uuid.hpp"
-#include <time.h>
+#include <chrono>
 #include <limits>
 #include <random>
 #include <thread>
 
 namespace odb::internal {
 
-static auto random_uuid()
+/// Creates a random number generator with a seed that will be different for each thread
+static std::mt19937 create_rng()
 {
-    static thread_local std::mt19937 generator{0 /*std::clock()+ std::this_thread::get_id()._Id*/}; // TODO
+    std::seed_seq seeds{std::hash<std::thread::id>{}(std::this_thread::get_id()),                                   // thread::get_id() is unique for each std::thread and std::jthread, but if a thread finishes then the value can be reused for another thread
+                        static_cast<size_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count())}; // this is why we also take the time into account for our seed
+    return std::mt19937{seeds};
+}
+
+static Uuid::StorageType random_uuid()
+{
+    static thread_local std::mt19937 generator{create_rng()};
 
     static thread_local std::uniform_int_distribution<Uuid::StorageType> distribution(
         std::numeric_limits<Uuid::StorageType>::min(),

4450e14d6d1d75621b5e03f963d4710f2dd4d16a
Jules Fouchy <jules.fouchy@ntymail.com>
2022-11-06 15:12:36 +0100

    🤏 [AnyId] Add some constructors and assignment from Id<T>

--- src/AnyId.hpp
+++ src/AnyId.hpp
@@ -14,6 +14,22 @@ public:
     AnyId(Id<T> const& id)
         : _uuid{id._uuid}
     {}
+    template<typename T>
+    AnyId(Id<T>&& id) noexcept
+        : _uuid{std::move(id._uuid)}
+    {}
+    template<typename T>
+    auto operator=(Id<T> const& id) -> AnyId&
+    {
+        _uuid = id._uuid;
+        return *this;
+    }
+    template<typename T>
+    auto operator=(Id<T>&& id) noexcept -> AnyId&
+    {
+        _uuid = std::move(id._uuid);
+        return *this;
+    }
 
     template<typename T>
     operator Id<T>() const
@@ -21,10 +37,21 @@ public:
         return Id<T>{_uuid};
     }
 
-    friend bool operator==(const AnyId&, const AnyId&) = default;
+    friend auto operator==(AnyId const&, AnyId const&) -> bool = default;
+
+    template<typename T>
+    friend auto operator==(AnyId const& id1, Id<T> const& id2) -> bool
+    {
+        return id1 == AnyId{id2};
+    }
+    template<typename T>
+    friend auto operator==(Id<T> const& id1, AnyId const& id2) -> bool
+    {
+        return id2 == id1;
+    }
 
     auto underlying_uuid() -> uuids::uuid& { return _uuid; }
-    auto underlying_uuid() const -> const uuids::uuid& { return _uuid; }
+    auto underlying_uuid() const -> uuids::uuid const& { return _uuid; }
 
 private:
     friend std::hash<AnyId>;
